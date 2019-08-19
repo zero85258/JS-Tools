c2517e40fd9dd4b5d25e0391d8c89f60f4de74e9
yufufu <zero85258@adbert.com.tw>
2019-08-19 16:05:11 +0800

    <feature>:

---
+++ tree-traversal.js
@@ -0,0 +1,28 @@
+json = [
+  [
+    "rudimental fill",
+    "rudimental rhythm",
+    "fill rhythm",
+    "rhythm fill",
+    "straight rhythm",
+    "straight fill",
+    {
+	"gsdgsdg":["gststdfd"]
+    }
+  ],
+  "is對稱?",
+  "tuplet?",
+  "密集and分散",
+  "polyfy",
+  "is和協?",
+  "碎?",
+  "[tone,symbal]多與少",
+  "快慢",
+  "滿",
+  "感"
+]
+
+// 樹狀走訪器
+Object.entries(json).forEach((key, value) => {
+	console.log(key,value);
+});
